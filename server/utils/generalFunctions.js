c5386c33b5d133b09ef1913310f64c4449e8fb06
Omer <omerg863@gmail.com>
2023-11-07 19:43:17 +0200

    fix send email

--- server/utils/generalFunctions.js
+++ server/utils/generalFunctions.js
@@ -4,7 +4,7 @@ export const sendEmail = async (reciever, subject, text) => {
     var transporter = createTransport({
         service: process.env.EMAIL_SERVICE,
         auth: {
-        user: process.env.EMAIL_ADDRESS,
+        user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD
         }
     });
