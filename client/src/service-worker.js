212c33e570c3008b84c2f544bd5b17d2406c221c
Omer Gaiznger <Omerg863@gmail.com>
2024-01-01 15:18:52 +0200

    service worker update

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -25,6 +25,43 @@ precacheAndRoute(self.__WB_MANIFEST);
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+registerRoute(
+  // Return false to exempt requests from being fulfilled by index.html.
+  ({ request, url }) => {
+    // If this isn't a navigation, skip.
+    if (request.mode !== 'navigate') {
+      return false;
+    } // If this is a URL that starts with /_, skip.
+
+    if (url.pathname.startsWith('/_')) {
+      return false;
+    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+
+    if (url.pathname.match(fileExtensionRegexp)) {
+      return false;
+    } // Return true to signal that we want to use the handler.
+
+    return true;
+  },
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+);
+
+
+// Check if the request is in the cache, if not, fetch it from the network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
@@ -48,19 +85,17 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// Any other custom service worker logic can go here.
-
 const deleteCache = async (key) => {
   await caches.delete(key);
 };
 
 const deleteOldCaches = async () => {
   const keyList = await caches.keys();
-  console.log(keyList);
   await Promise.all(keyList.map(deleteCache));
 };
 
 self.addEventListener("activate", (event) => {
-  console.log("activate");
   event.waitUntil(deleteOldCaches());
 });
+
+// Any other custom service worker logic can go here.
